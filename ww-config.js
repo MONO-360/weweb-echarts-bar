0cbb1081092c52326cbed393c7a1f549b0e235c9
MONO360 <vanessa.le82@googlemail.com>
2026-04-28 12:23:28 +0800

    Create ww-config.js

---
+++ ww-config.js
@@ -0,0 +1,14 @@
+export default {
+  editor: {
+    label: 'ECharts Bar Chart',
+    icon: 'bar-chart',
+  },
+  properties: {
+    data: {
+      label: 'Chart Data',
+      type: 'Array',
+      defaultValue: [],
+      bindable: true,
+    },
+  },
+}
