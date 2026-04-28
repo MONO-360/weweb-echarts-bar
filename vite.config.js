eb4d8c3e3b3a4fab53f56092e7eb07d1c5a18b89
MONO360 <vanessa.le82@googlemail.com>
2026-04-28 13:34:19 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+
 export default defineConfig({
   plugins: [vue()],
   build: {
@@ -7,13 +8,7 @@ export default defineConfig({
       entry: 'src/wwElement.vue',
       name: 'EChartsBar',
       fileName: 'weweb-echarts-bar',
-      formats: ['es', 'umd']
-    },
-    rollupOptions: {
-      external: ['vue'],
-      output: {
-        globals: { vue: 'Vue' }
-      }
+      formats: ['umd']
     }
   }
 })
