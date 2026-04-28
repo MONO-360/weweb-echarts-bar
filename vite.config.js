87691a4c58e3af87f92363202c1cba515afd8220
MONO360 <vanessa.le82@googlemail.com>
2026-04-28 13:12:06 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
 export default defineConfig({
   plugins: [vue()],
   build: {
@@ -11,13 +10,9 @@ export default defineConfig({
       formats: ['es', 'umd']
     },
     rollupOptions: {
-      external: ['vue', 'echarts', 'vue-echarts'],
+      external: ['vue'],
       output: {
-        globals: {
-          vue: 'Vue',
-          echarts: 'echarts',
-          'vue-echarts': 'VueECharts'
-        }
+        globals: { vue: 'Vue' }
       }
     }
   }
